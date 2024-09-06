df4fd6ddbca0512ec81dcd398528fd330cddda0f
madicke12 <cissemadicke8@gmail.com>
2024-09-06 22:57:45 +0000

    Add chevron image and board title to page component

--- src/app/board/page.tsx
+++ src/app/board/page.tsx
@@ -1,10 +1,20 @@
 import Navbar from '@/components/navbar'
 import React from 'react'
+import chevrn from '../../../public/1x/chevron-down.png'
+import Image from 'next/image'
 
 export default function page() {
   return (
     <div>
         <Navbar />
+        <div>
+            <div className ='flex'>
+                <Image src={chevrn} alt='chevron' width={20} height={20} />
+                <span className=''>
+                    Board
+                </span>
+            </div>
+        </div>
     </div>
   )
 }
