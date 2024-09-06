08d7f0071c5d4742c4c6bafa1b43088f990c8433
madicke12 <cissemadicke8@gmail.com>
2024-09-06 21:13:41 +0000

    Refactor SideNav component: Update layout and styles
    - Updated the layout and styles of the SideNav component to improve the appearance and user experience.
    - Added a new import for the usePathname hook from 'next/navigation' to track the current pathname.
    - Updated the class names and styles for the SideNav component to match the design requirements.
    - Updated the Link components for the Board and Settings pages to highlight the active page based on the current pathname.

--- src/components/sidenav.tsx
+++ src/components/sidenav.tsx
@@ -5,22 +5,25 @@ import Image from 'next/image';
 import Logo from '../../public/logo.svg'
 import Link from 'next/link';
 import board from  '../../public/1x/wireframe.png'
+import { usePathname } from 'next/navigation';
 
 const SideNav =()=>{
-    
+    const pathName = usePathname();
+    console.log(pathName)
     return (
-        <div className=" w-[252px] h-[100vh] bg-white flex flex-col">
-            <div className="w-44 h-12 flex gap-2 mt-4 ml-4  items-center">
+        <div className=" w-64 h-[100vh] bg-white flex flex-col border border-secondary-100">
+            <div className={`w-44 h-12 flex gap-2 mt-4 mb-4 ml-4  items-center `}>
                 <Image src={Logo} alt="logo" width={44} height={44} className=' rounded-[4px]' />
-                <span className=" text-black font-bold text-3xl ">Kanban</span>
+                <span className="text-black  font-bold text-3xl ">Kanban</span>
             </div>
-            <div className='flex flex-col w-64 h-[248px gap-1 leading-3'>
-                <Link href='board' className='p-4 flex items-center justify-start gap-2'>
-                    <Image src={board} className='' width={20} height={20} alt='board image' />
-                    <span className='text-black font-bold text-sm'>Board</span>
+            <div className={`flex flex-col w-full h-[248px] gap-1 leading-3 `}>
+                <Link href='board' className={`p-4 flex items-center justify-start gap-2 ${pathName=='/board'?'bg-primary-200' : 'text-black'} hover:bg-primary-200`}>
+                    <Image src={board}  width={20} height={20} alt='board image' />
+                    <span className=' font-bold text-sm text-black'>Board</span>
                 </Link>
-                <Link href='/settings' className='p-4 flex items-center justify-start'>
+                <Link href='/settings' className={`p-4 flex items-center justify-start hover:bg-primary-200 gap-2 ${pathName=='/settings'?'bg-primary-200': ''}`}>
                     <Image src={board} className='' width={20} height={20} alt='board image' />
+                    <span className=' font-bold text-sm text-black'>Settings</span>
                 </Link>
             </div>
             
