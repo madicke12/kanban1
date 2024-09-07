04c1ca65b57a70c087299ffbab694286dc382832
madicke12 <cissemadicke8@gmail.com>
2024-09-07 17:30:10 +0000

    Refactor: Remove unused code and console logs in board page component

--- src/app/board/page.tsx
+++ src/app/board/page.tsx
@@ -1,18 +1,14 @@
-import { BoardSelect } from '@/components/boardDropDown'
-import Navbar from '@/components/navbar'
-import prisma from '../lib/prismaSingleton'
+'use client'
+import { useSearchParams } from "next/navigation"
 
-export default async function page() {
-    const Board= await prisma.board.findMany()
-    const boardListe= Board.map(item=>{return ({name:item.name , id:item.id})})
-    console.log(boardListe)
+export default  function Page() {
+const params = useSearchParams()
+console.log(params.get('id'))
   return (
     <div>
-        <Navbar />
         <div>
             <div className ='flex gap-2 p-3 mt-4 '>
-                <BoardSelect bListe={boardListe} />
-                {/* <Image src={chevrn} alt='chevron' width={30} height={30} /> */}
+                    <h1>working</h1>
             </div>
         </div>
     </div>
