5ac048f01236fa6398cf559eadf2ca9f77ed5dca
madicke12 <cissemadicke8@gmail.com>
2024-09-07 17:30:35 +0000

    Refactor: Add board dropdown menu and filter button to board layout

--- src/components/boardDropDown.tsx
+++ src/components/boardDropDown.tsx
@@ -1,36 +1,39 @@
+'use client'
 import * as React from "react"
 
 import {
-  Select,
-  SelectContent,
-  SelectGroup,
-  SelectItem,
-  SelectLabel,
-  SelectTrigger,
-  SelectValue,
+    Select,
+    SelectContent,
+    SelectGroup,
+    SelectItem,
+    SelectLabel,
+    SelectTrigger,
+    SelectValue,
 } from "@/components/ui/select"
 import { BoardListeType } from "@/app/lib/types/itemTypes"
+import { useRouter } from "next/navigation"
 
 
 
-export function BoardSelect({bListe}: BoardListeType) {
-    const Element= bListe.map(item=>{return <SelectItem key={item.id} value={item.id}>{item.name}</SelectItem>})
-  return (
-    <Select>
-      <SelectTrigger className="w-[180px] ">
-        <SelectValue placeholder="Select a Board" />
-      </SelectTrigger>
-      <SelectContent>
-        <SelectGroup>
-          <SelectLabel>Boards</SelectLabel>
-            {Element}
-          <SelectItem value="apple">Apple</SelectItem>
-          <SelectItem value="banana">Banana</SelectItem>
-          <SelectItem value="blueberry">Blueberry</SelectItem>
-          <SelectItem value="grapes">Grapes</SelectItem>
-          <SelectItem value="pineapple">Pineapple</SelectItem>
-        </SelectGroup>
-      </SelectContent>
-    </Select>
-  )
+export function BoardSelect({ bListe }: BoardListeType) {
+    const Element = bListe.map(item => { return  <SelectItem key={item.id} className="cursor-pointer" value={item.id}> {item.name}</SelectItem> })
+    const router = useRouter();
+
+    const handleValueChange = (value: string) => {
+        router.push(`/board/${value}`);
+    };
+    return (
+        <Select onValueChange={handleValueChange}>
+            <SelectTrigger className="w-[180px] "> 
+                <SelectValue placeholder="Select a Board" />
+            </SelectTrigger>
+            <SelectContent>
+                <SelectGroup>
+                    <SelectLabel>Boards</SelectLabel>
+                    {Element}
+                </SelectGroup>
+            </SelectContent>
+        </Select>
+    )
 }
+ 
