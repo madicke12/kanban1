223aecc003726fa96bcd8e2caf4ef53dda3f39d0
madicke12 <cissemadicke8@gmail.com>
2024-09-08 16:48:32 +0000

    Refactor: Update column component to display tasks and add "Add task" button

--- src/components/column.tsx
+++ src/components/column.tsx
@@ -1,16 +1,25 @@
+import { C } from "@/app/lib/types/itemTypes"
 import TaskCard from "./taskCard"
+import { Button } from "./ui/button"
 
-const ColumN= ()=>{
-    return(
-        <div className="w-[288px] h-fit rounded-[6px] p-[10px]">
-           <div className="flex items-center justify-between">
-           <div className="text-xl font-bold">name</div>
-           <span className="font-bold">...</span>
-           </div>
-           <div className=" bg-madicke">
-            <TaskCard/>
-           </div>
+const ColumN = ({ columns }: {columns:C}) => {
+    const TaskELements = columns[0].Task?.map(item => { return <TaskCard key={item.id} task ={item} /> })
+    console.log('a',columns)
+    return (
+        <div className="w-[288px]  ">
+            <div className="flex items-center justify-between">
+                <div className="text-xl font-bold">{columns[0].name}</div>
+                <span className="font-bold">...</span>
+            </div>
+            <div className=" bg-madicke h-fit rounded-[6px] p-[10px]">
+
+                <div className=" bg-madicke flex flex-col gap-3">
+                    {TaskELements}
+                </div>
+            </div>
+            <Button className="w-full bg-transparent text-gray-300 hover:bg-transparent hover:outline-dashed  mt-4"> + Add task</Button>
         </div>
+
     )
 }
 
--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -5,7 +5,7 @@ import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar"
 
 const Navbar = () => {
     return (
-        <nav className="flex items-center justify-between border-b border-secondary-100 h-16 w-full bg-white ">
+        <nav className="flex items-center justify-between   sticky top-0 border-b border-secondary-100 h-16 w-full bg-white ">
             <div className=" items-center p-2 ml-3 gap-1   bg-gray-100 rounded-sm flex">
                 <Image src={searchIcon} alt="search" width={20} height={20} />
                 <input type="text" placeholder='Search...' className="bg-transparent z-0 h-5 outline-none"></input>
