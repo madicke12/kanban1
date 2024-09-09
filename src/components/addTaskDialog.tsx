431b8acb3e5ee903cec1787330cb9a6890b6a800
madicke12 <cissemadicke8@gmail.com>
2024-09-09 17:30:31 +0000

    Add AddTaskDialog component

---
+++ src/components/addTaskDialog.tsx
@@ -0,0 +1,19 @@
+import { Button } from "@/components/ui/button";
+import { Dialog, DialogContent, DialogTrigger } from "@/components/ui/dialog";
+import { PlusIcon } from "@heroicons/react/24/solid";
+import { AddBoardForm } from "./addBoardForm";
+
+export async function DialogDemo() {
+  return (
+    <Dialog>
+      <DialogTrigger asChild>
+        <Button variant="outline">
+          <PlusIcon height={20} /> Add new Board
+        </Button>
+      </DialogTrigger>
+      <DialogContent className="sm:max-w-[425px]">
+        <AddBoardForm/>
+      </DialogContent>
+    </Dialog>
+  );
+}
