78ef4a09ff8eb614c0e6b8680b499b8e99fa6646
madicke12 <cissemadicke8@gmail.com>
2024-09-09 17:30:20 +0000

    Refactor: Add AddBoardForm component

---
+++ src/components/addBoardForm.tsx
@@ -0,0 +1,50 @@
+import { Button } from "@/components/ui/button";
+import {
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+} from "@/components/ui/dialog";
+import { Input } from "./ui/input";
+import { Label } from "./ui/label";
+
+// import { getServerSession } from "next-auth";
+// import { submit } from "../../lib/actions/actions";
+// import { authOption } from "../api/auth/[...nextauth]/route";
+
+ export const AddBoardForm = async () => {
+  //const session =  await getServerSession(authOption)
+  //console.log(session)
+  return (
+    <form  method="post">
+      <DialogHeader>
+        <DialogTitle>Add new Board</DialogTitle>
+        <DialogDescription>
+          Add a new board here click Create new Board when you're done
+        </DialogDescription>
+      </DialogHeader>
+      <div className="grid gap-4 py-4">
+        <div className="grid grid-cols-1 items-center gap-4">
+          <Label htmlFor="Boardname" className="">
+            Board Name
+          </Label>
+          <Input
+            name="boardName"
+            id="Boardname"
+            placeholder="eg: Marketing Plan "
+            className="col-span-3"
+          />
+        </div>
+        <div className="flex flex-col   mt-4 gap-4">
+          <input className="hidden" value={'656f4da9e7241b17b75896bc'} name="userId"/>
+         <input type="text" className="hidden" value={JSON.stringify(['To Do','Doing','Done'])} readOnly name="columns" />
+        </div>
+      </div>
+      <DialogFooter>
+        <Button className="w-full dark:hover:bg-white dark:hover:text-black" type="submit">
+          Create new Board
+        </Button>
+      </DialogFooter>
+    </form>
+  );
+};
