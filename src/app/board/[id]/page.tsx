241d6ac2a3db86aa943539365ff5ab0542fa08f8
madicke12 <cissemadicke8@gmail.com>
2024-09-10 02:14:49 +0000

    Refactor: Update page component to use Board component for rendering columns

--- src/app/board/[id]/page.tsx
+++ src/app/board/[id]/page.tsx
@@ -1,28 +1,17 @@
-import prisma from '../../lib/prismaSingleton'
-import React from 'react'
-import { ParamsType } from '../../lib/types/itemTypes'
-import ColumN from '../../../components/column'
-
+import Board from '@/components/board';
+import prisma from '../../lib/prismaSingleton';
+import { ParamsType } from '../../lib/types/itemTypes';
 
 export default async function page({ params }: { params: ParamsType }) {
     const boardData = await prisma.board.findUnique({
-        where: {
-            id: params.id
-        },
+        where: { id: params.id },
         include: {
             columns: { include: { Task: { include: { Subtasks: true } } } },
-        }
-    }
-    ) 
-    const todos = boardData?.columns.filter(item=>item.name === 'To Do')
-    const inProgress = boardData?.columns.filter(item=>item.name === 'Doing')
-    const done = boardData?.columns.filter(item=>item.name === 'Done')
+        },
+    });
     return (
-        <div className='p-3 flex gap-3 overflow overflow-y-auto'>
-            <ColumN columns={todos}/>
-            <ColumN columns={inProgress}/>
-            <ColumN columns={done}/>
-            <ColumN columns={done}/>
-        </div>
-    )
+     <div className=''>
+        <Board boardData={boardData?.columns} />
+  </div>
+    );
 }
