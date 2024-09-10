100d80d3a446bcfafb4c7bbf9a7c049aa64b33c3
madicke12 <cissemadicke8@gmail.com>
2024-09-10 16:53:09 +0000

    Refactor: Add AddTask component for adding a new task to a board

---
+++ src/components/addTask.tsx
@@ -0,0 +1,67 @@
+
+import { Button } from "@/components/ui/button";
+import {
+    Dialog,
+    DialogContent,
+    DialogFooter,
+    DialogHeader,
+    DialogTitle,
+    DialogTrigger
+} from "@/components/ui/dialog";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+import { Textarea } from "@/components/ui/textarea";
+import DynamicInput from "./dynamic-input";
+import StatusSelect from "./select";
+// import { createTask } from "@/lib/actions/actions";
+
+const AddTask = ({id}: {id: string}) => {
+  return (
+    <Dialog  >
+      <DialogTrigger className="w-full">
+      <Button className="w-full bg-transparent text-gray-300 hover:bg-transparent hover:outline-dashed mt-4">
+        + Add task
+      </Button>
+      </DialogTrigger>
+      <DialogContent>
+        <DialogHeader>
+          <DialogTitle>Add New Task</DialogTitle>
+        </DialogHeader>
+        <form >
+          <div className="mb-3">
+            <Label htmlFor="name">Task Name</Label>
+            <Input
+              id="name"
+              className="mt-2"
+              placeholder="e.g Do my homework"
+              name="taskName"
+            />
+          </div>
+          <div className="mb-3">
+            <Label htmlFor="description" className="mb-2">
+              Description
+            </Label>
+            <Textarea id="description" className="mt-2" placeholder="" name="description" />
+          </div>
+          <div className="flex flex-col">
+            <Label className="mb-2">Subtasks</Label>
+            <DynamicInput/>
+            <div className="mt-2">
+            <Label className='mb-2'>Current Status</Label>
+            <StatusSelect/>
+            </div>
+          </div>
+          <input type="text" value={'656f4da9e7241b17b75896bc'} className="hidden" name="userId" />
+          <input type="text" value={id} className="hidden" name="columnId" />
+      <DialogFooter>
+        <Button className="w-full mt-2 bg-primary-450 hover:bg-primary" type="submit">
+          Create new Task
+        </Button>
+      </DialogFooter>
+        </form>
+      </DialogContent>
+    </Dialog>
+  );
+};
+
+export default AddTask;
