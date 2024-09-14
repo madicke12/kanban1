f3b5ce75747e2f4293539a3e86cb8604aa4d426c
madicke12 <cissemadicke8@gmail.com>
2024-09-14 21:19:02 +0000

    Refactor layout.tsx to remove unused import and clean up code

--- src/app/board/layout.tsx
+++ src/app/board/layout.tsx
@@ -6,18 +6,20 @@ import Image from 'next/image';
 import { PropsWithChildren } from "react";
 import filterIcon from '../../../public/1x/ios/@3x/filter-2@3x.png';
 import prisma from '../lib/prismaSingleton';
+import { BoardProvider } from '../boardContext';
 
 const layout = async (props: PropsWithChildren) => {
-  const Board = await prisma.board.findMany()
-  const boardListe = Board.map(item => { return ({ name: item.name, id: item.id }) })
-  console.log(boardListe)
+  // const Board = await prisma.board.findMany()
+  // const boardListe = Board.map(item => { return ({ name: item.name, id: item.id }) })
+  // console.log(boardListe)
   return (
+    <BoardProvider >
     <div className="flex " >
       <SideNav />
       <div className="w-full">
         <Navbar />
         <div className='flex gap-2 p-3 mt-4  items-center justify-between w-full '>
-          <BoardSelect bListe={boardListe} />
+          <BoardSelect  />
           <div className=" ">
             <Button className="hover:bg-transparent outline outline-gray-100 bg-transparent text-black gap-2" > <Image src={filterIcon} alt='' width={14} height={14} /> 
               <span className="">filter</span>
@@ -27,6 +29,7 @@ const layout = async (props: PropsWithChildren) => {
         {props.children}
       </div>
     </div>
+    </BoardProvider>
 
   );
 };
