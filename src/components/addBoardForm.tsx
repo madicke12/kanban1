603588e2968f1b43389119223b9c69b125cd9b93
madicke12 <cissemadicke8@gmail.com>
2024-09-18 16:19:36 +0000

    Refactor addBoardForm component to use boardContext for dispatch and handle input changes

--- src/components/addBoardForm.tsx
+++ src/components/addBoardForm.tsx
@@ -1,4 +1,5 @@
 /* eslint-disable react/no-unescaped-entities */
+'use client'
 import { Button } from "@/components/ui/button";
 import {
   DialogDescription,
@@ -9,12 +10,21 @@ import {
 import { Input } from "./ui/input";
 import { Label } from "./ui/label";
 import DynamicInput from "./dynamic-input";
+import { useBoardDispatch } from "@/app/boardContext";
+import { useState } from "react";
 
 // import { getServerSession } from "next-auth";
 // import { submit } from "../../lib/actions/actions";
 // import { authOption } from "../api/auth/[...nextauth]/route";
 
  export const AddBoardForm =  () => {
+  const dispatch = useBoardDispatch()
+  const [board, setBoard] = useState({id:'adkjabdkabda', name:'',userId:'',columns:[]})
+  const handleChange = (e:React.ChangeEvent<HTMLInputElement>)=>{
+
+    setBoard({...board,[e.target.name]:e.target.value})
+  }
+
   //const session =  await getServerSession(authOption)
   //console.log(session)
   return (
@@ -27,24 +37,25 @@ import DynamicInput from "./dynamic-input";
       </DialogHeader>
       <div className="grid gap-4 py-4">
         <div className="grid grid-cols-1 items-center gap-4">
-          <Label htmlFor="Boardname" className="">
+          <Label htmlFor="name" className="">
             Board Name
           </Label>
           <Input
-            name="boardName"
-            id="Boardname"
+            name="name"
+            id="name"
             placeholder="eg: Marketing Plan "
             className="col-span-3"
+            onChange={handleChange}
           />
         </div>
         <div className="flex flex-col   mt-4 gap-4">
-          <input className="hidden" value={'656f4da9e7241b17b75896bc'} name="userId"/>
-          <DynamicInput type={'column'}/>
+          <input className="hidden" value={'656f4da9e7241b17b75896bc'} onChange={handleChange} name="userId"/>
+          <DynamicInput type={'column'} handleChanges={handleChange}/>
          {/* <input type="text" className="hidden" value={JSON.stringify(['To Do','Doing','Done'])} readOnly name="columns" /> */}
         </div>
       </div>
       <DialogFooter>
-        <Button className="w-full bg-primary hover:bg-primary-500" type="submit">
+        <Button className="w-full bg-primary hover:bg-primary-500" type="button" onClick={()=> dispatch({type:'added',board})}>
           Create new Board
         </Button>
       </DialogFooter>
