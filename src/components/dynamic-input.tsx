a54c5dd9222c2b503bdd57af4c8a3bfbf7a91b08
madicke12 <cissemadicke8@gmail.com>
2024-09-18 16:19:29 +0000

    Refactor DynamicInput component to handle input changes

--- src/components/dynamic-input.tsx
+++ src/components/dynamic-input.tsx
@@ -5,7 +5,7 @@ import { Input } from "@/components/ui/input";
 import { PlusIcon, XMarkIcon } from "@heroicons/react/24/solid";
 import { useState } from "react";
 
-const DynamicInput = ({ type }:any) => {
+const DynamicInput = ({ type , handleChanges }:any) => {
   const [inputCount, setInputCount] = useState([
     { id: Date.now() * 2, value: "" },
   ]);
@@ -44,7 +44,7 @@ const DynamicInput = ({ type }:any) => {
           </Button>
         </div>
       ))}
-      <Input name={`${type==='column' ? 'columns' :'subtasks'}`} value={JSON.stringify(inputCount)} className="hidden"/>
+      <Input onChange={handleChanges} name={`${type==='column' ? 'columns' :'subtasks'}`} value={JSON.stringify(inputCount)} className="hidden"/>
       <Button
         type="button"
         onClick={handleAddInput}
