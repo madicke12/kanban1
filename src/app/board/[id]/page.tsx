dfbbb6d5bc3378df7f39c909bf021c11585e279d
madicke12 <cissemadicke8@gmail.com>
2024-09-22 21:42:12 +0000

    Refactor board page to pass board id as a prop to the Board component

--- src/app/board/[id]/page.tsx
+++ src/app/board/[id]/page.tsx
@@ -1,17 +1,11 @@
+'use client'
 import Board from '@/components/board';
-import prisma from '../../lib/prismaSingleton';
 import { ParamsType } from '../../lib/types/itemTypes';
 
-export default async function page({ params }: { params: ParamsType }) {
-    const boardData = await prisma.board.findUnique({
-        where: { id: params.id },
-        include: {
-            columns: { include: { Task: { include: { Subtasks: true } } } },
-        },
-    });
+export default  function page({ params }: { params: ParamsType }) {
     return (
      <div className=''>
-        <Board boardData={boardData?.columns} />
+        <Board id={params.id} />
   </div>
     );
 }
