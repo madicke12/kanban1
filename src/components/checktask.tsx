fdcb7a8fff64b9d8f59e27244bd1174270f859b4
madicke12 <cissemadicke8@gmail.com>
2024-09-26 20:25:11 +0000

    Refactor checktask component to use Prisma for updating subtask's isDone field

--- src/components/checktask.tsx
+++ src/components/checktask.tsx
@@ -1,21 +1,24 @@
 'use client'
+import { useSubTaskDispatch } from "@/app/boardContext";
 import { SubtaskType } from "@/app/lib/types/itemTypes";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
+import axios from "axios";
 
 import React, { useState } from 'react';
 
 const CheckTask = ({ cisse  }:{cisse :SubtaskType}) => {
   const [isChecked, setChecked] = useState(false);
-
-  const handleClick = () => {
+  const subdispatch = useSubTaskDispatch()
+  const handleClick = async () => {
     setChecked(!isChecked);
-    const form = document.getElementById('samaForm') as HTMLFormElement  ;
-    form.requestSubmit();
+   const a = (await axios.put('/api/subtask/update', { subtaskId: cisse.id , isDone:isChecked})).data;
+    if(a.status === 200){
+      subdispatch({type:'update',subtask:a.response})
+    }
   };
 
   return (
-    <form  id="samaForm">
       <div
         className="bg-primary-200 hover:bg-primary-250  rounded-sm px-1 mt-2 flex items-center"
         onClick={handleClick}
@@ -31,7 +34,6 @@ const CheckTask = ({ cisse  }:{cisse :SubtaskType}) => {
         />
         <Label htmlFor="task" className="font-bold">{cisse.name}</Label>
       </div>
-    </form>
   );
 };
 
