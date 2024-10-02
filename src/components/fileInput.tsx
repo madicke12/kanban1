7b4ca59e52acdfc5fcbcf64e4d0b1f29a3d93696
madicke12 <cissemadicke8@gmail.com>
2024-10-02 14:52:11 +0000

    Refactor fileInput component to update label text from "Picture" to "Photo" and add a ref prop for file input

--- src/components/fileInput.tsx
+++ src/components/fileInput.tsx
@@ -1,11 +1,11 @@
 import { Input } from "@/components/ui/input"
 import { Label } from "@/components/ui/label"
 
-export function InputFile() {
+export function InputFile({ref}:{ref: React.RefObject<HTMLInputElement>}) {
   return (
     <div className="grid w-full max-w-sm items-center gap-1.5">
-      <Label htmlFor="picture">Picture</Label>
-      <Input id="picture" type="file" />
+      <Label htmlFor="picture">Photo <span>Facultative</span></Label>
+      <Input id="picture" ref={ref} type="file" />
     </div>
   )
 }
