91273283c08310a717d62af78c7c8447b847cdfc
madicke12 <cissemadicke8@gmail.com>
2024-10-03 22:22:38 +0000

    Refactor create task route to include subtasks and handle image upload

--- src/app/api/task/create/route.ts
+++ src/app/api/task/create/route.ts
@@ -1,9 +1,36 @@
+import prisma from "@/app/lib/prismaSingleton";
 import { NextResponse } from "next/server"
 
 export async function POST(req:Request){
     const body = await req.json()
     const {titre, description, picture,columnId ,currentstatus , subtasks , userId  } = body
-    
 
-    return NextResponse.json({message:'task created'})
+    const Subtasks = JSON.parse(subtasks.toString()).map(
+        (item:{id:string,value:string}) => item.value
+      );
+
+    console.log({titre, description, picture,columnId ,currentstatus , Subtasks , userId})
+
+    try{
+        const task  = await prisma.task.create({
+            data:{
+                titre,
+                description,
+                picture,
+                columnId,
+                currentStatus: currentstatus,
+                Subtasks:{
+                    create: Subtasks.map((item:string) => ({
+                        name: item,
+                        isDone: false,
+                    })),
+                }
+            }
+        })
+        return NextResponse.json(task,{status:200})
+    }catch(err){
+        console.log(err)
+        return NextResponse.json({status:500})
+    }
+
 }
