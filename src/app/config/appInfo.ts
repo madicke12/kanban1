d58ed88cb46194f99055be9219fb63552d5778d4
madicke12 <cissemadicke8@gmail.com>
2024-10-04 17:30:58 +0000

    Add appInfo configuration file

---
+++ src/app/config/appInfo.ts
@@ -0,0 +1,10 @@
+
+export const appInfo = {
+    // learn more about this on https://supertokens.com/docs/thirdpartyemailpassword/appinfo
+    appName: "kanban1",
+    apiDomain: "http://localhost:3000",
+    websiteDomain: "http://localhost:3000",
+    apiBasePath: "/api/auth",
+    websiteBasePath: "/auth"
+  }
+  
