77448b33f14f232da4f3947412cd65f91072748c
madicke12 <cissemadicke8@gmail.com>
2024-10-12 16:19:44 +0000

    Refactor page component to remove unused SessionAuth component

--- src/app/board/[id]/page.tsx
+++ src/app/board/[id]/page.tsx
@@ -1,15 +1,12 @@
-'use client'
+
 import Board from '@/components/board';
 import { ParamsType } from '../../lib/types/itemTypes';
-import { SessionAuth } from "supertokens-auth-react/recipe/session"
 
 export default function page({ params }: { params: ParamsType }) {
   return (
     <div className=''>
-      <SessionAuth>
 
         <Board id={params.id} />
-      </SessionAuth>
     </div>
   );
 }
