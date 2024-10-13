7da6509025262739e2b9fb3f9489e86b9711c180
madicke12 <cissemadicke8@gmail.com>
2024-10-13 23:15:54 +0000

    Refactor NextAuth configuration to remove unused GitHub authentication provider

---
+++ src/app/lib/auth.ts
@@ -0,0 +1,9 @@
+import NextAuth from "next-auth";
+import GithubProvider from "next-auth/providers/github";
+import { PrismaAdapter } from "@next-auth/prisma-adapter";
+import prisma from "./prismaSingleton";
+
+
+export const {
+
+}
