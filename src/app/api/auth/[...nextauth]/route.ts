44876069591ad99a3505f589e80da037ba53c1e1
madicke12 <cissemadicke8@gmail.com>
2024-10-15 16:09:17 +0000

    Refactor NextAuth configuration to add Google authentication provider and update session callback

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -12,7 +12,12 @@ const handler = NextAuth({
             clientId: process.env.GOOGLE_CLIENT_ID as string,
             clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
           })
-    ],
-})
+    ], callbacks: {
+        session: ({ session, user }: { session: any, user: any }) => ({
+          ...session,
+          user,
+        }),
+      },
+    });
 
 export { handler as GET, handler as POST }
