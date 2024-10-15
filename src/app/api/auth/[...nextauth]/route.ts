c6c30111d8abc18f36d76934d6c1fadb20f4b428
madicke12 <cissemadicke8@gmail.com>
2024-10-15 15:32:53 +0000

    Refactor NextAuth configuration to add Google authentication provider and update GitHub provider

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,12 +1,17 @@
 import NextAuth from "next-auth"
-import { GitHub } from "next-auth/providers"
+import GitHub from "next-auth/providers/github"
+import GoogleProvider from "next-auth/providers/google"
 
 const handler = NextAuth({
     providers: [
         GitHub({
-            clientId: process.env.GITHUB_ID,
-            clientSecret: process.env.GITHUB_SECRET,
+            clientId: process.env.GITHUB_ID as string,
+            clientSecret: process.env.GITHUB_SECRET as string,
         }),
+        GoogleProvider({
+            clientId: process.env.GOOGLE_CLIENT_ID as string,
+            clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
+          })
     ],
 })
 
