fe375141ad1000a6349be8f406f5e4828002896c
madicke12 <cissemadicke8@gmail.com>
2024-10-15 16:28:39 +0000

    Refactor delete board API route and update response handling

--- src/app/api/board/delete/route.ts
+++ src/app/api/board/delete/route.ts
@@ -1,3 +1,6 @@
+import { NextResponse } from "next/server"
+
 export async function POST(){
-    return {status: 200, body: {message: "Deleted"}}
+    const mes ='asas'
+    return new   NextResponse(mes , { status: 200 })
 }
