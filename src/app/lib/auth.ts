11c7f7e94721c4643f25f7911000ba4ec6317b38
madicke12 <cissemadicke8@gmail.com>
2024-10-15 16:34:40 +0000

    Remove unused auth.ts file

--- src/app/lib/auth.ts
+++
@@ -1,9 +0,0 @@
-import NextAuth from "next-auth";
-import GithubProvider from "next-auth/providers/github";
-import { PrismaAdapter } from "@next-auth/prisma-adapter";
-import prisma from "./prismaSingleton";
-
-
-export const {
-
-}
