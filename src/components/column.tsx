316579ff6790ac8bd39b687bfcbae091f3d385fe
madicke12 <cissemadicke8@gmail.com>
2024-10-15 17:11:21 +0000

    Refactor Column component and remove unused code

--- src/components/column.tsx
+++ src/components/column.tsx
@@ -1,9 +1,8 @@
 'use client'
 import { ColumnType, TaskType } from '@/app/lib/types/itemTypes';
-import React, { useState } from 'react';
+import React from 'react';
 import { useDrop } from 'react-dnd';
 import AddTask from './addTask';
-import TaskCard from "./taskCard";
 import TaskModal from './taskDialog';
 import { useTaskDispatch, useTaskListe } from '@/app/boardContext';
 
@@ -13,18 +12,17 @@ interface ColumnProps {
 }
 
 const Column: React.FC<ColumnProps> = ({ column, updateColumnTasks }) => {
-  const dispatch = useTaskDispatch()
-  const Tasks = useTaskListe()
-  const t =Tasks.filter((task: TaskType) => task.columnId === column.id);
-  console.log('Task', Tasks)
-  const [{ isOver }, drop] = useDrop(() => ({
+  const dispatch = useTaskDispatch();
+  const Tasks = useTaskListe();
+  const t = Tasks.filter((task: TaskType) => task.columnId === column.id);
+
+  const [{ isOver }, dropRef] = useDrop(() => ({
     accept: 'TASK',
     drop: (item: { task: TaskType }) => {
       if (item.task.columnId !== column.id) {
         updateColumnTasks(item.task, column.id);
-        item.task.columnId = column.id
-
-        dispatch({ type: 'update', task: item.task })
+        item.task.columnId = column.id;
+        dispatch({ type: 'update', task: item.task });
       }
     },
     collect: (monitor) => ({
@@ -39,15 +37,16 @@ const Column: React.FC<ColumnProps> = ({ column, updateColumnTasks }) => {
         <span className="font-bold">...</span>
       </div>
       <div className={`bg-madicke h-fit rounded-[6px] p-[10px] ${isOver ? 'bg-gray-200' : ''}`}>
-        <div className="bg-madicke flex flex-col gap-3 min-h-80" ref={drop}>
+        <div
+          className="bg-madicke flex flex-col gap-3 min-h-80"
+          ref={dropRef as unknown as React.LegacyRef<HTMLDivElement>} // Use LegacyRef for function refs
+        >
           {t.map(task => (
-            // <TaskCard key={task.id} task={task} />
             <TaskModal key={task.id} task={task} />
           ))}
         </div>
       </div>
-      <AddTask id={column.id}  />
-      
+      <AddTask id={column.id} />
     </div>
   );
 };
