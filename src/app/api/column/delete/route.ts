18fec9776964f720f0c176924fe16f2803908518
madicke12 <cissemadicke8@gmail.com>
2024-10-17 22:18:36 +0000

    Refactor column delete route to handle DELETE requests and delete the corresponding column from the database

--- src/app/api/column/delete/route.ts
+++ src/app/api/column/delete/route.ts
@@ -1,6 +1,14 @@
+import prisma from "@/app/lib/prismaSingleton"
 import { NextResponse } from "next/server"
 
-export async function POST(){
-    const mes ='asas'
-    return new   NextResponse(mes , { status: 200 })
+export async function DELETE(req :Request){
+    const body = await req.json()
+    console.log(body)
+    try {
+        const column = await prisma.column.delete({ where: { id: body.columnId} })
+        return NextResponse.json(column, { status: 200 })
+    }catch(e){
+        console.log(e)
+        return NextResponse.json({ status: 500 })
+    }
 }
