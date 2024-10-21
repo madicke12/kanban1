60cfcab808d4fd06e05877d5757410a74049842a
madicke12 <cissemadicke8@gmail.com>
2024-10-21 13:19:52 +0000

    Refactor drop component to use zustand for managing column state

--- src/components/drop.tsx
+++ src/components/drop.tsx
@@ -1,22 +1,21 @@
 'use client'
 
-import { useColumnDispatch } from '@/app/boardContext';
+import {  useColumnStore } from '@/app/boardContext';
 import { ColumnType } from '@/app/lib/types/itemTypes';
 import { DropdownMenu, DropdownMenuContent, DropdownMenuLabel, DropdownMenuRadioGroup, DropdownMenuRadioItem, DropdownMenuSeparator, DropdownMenuTrigger } from './ui/dropdown-menu';
 import { useState } from 'react';
 import axios from 'axios';
 import { Button } from './ui/button';
-import { revalidatePath } from 'next/cache';
 
 
 
 export function DropdownMenuRadioGroupDemo({ column }: { column: ColumnType }) {
   const [position, setPosition] = useState("bottom")
-  const dispatch = useColumnDispatch()
+  const updateColumn = useColumnStore((state) => state.updateColumn);
   const deleteColumn = async (id:string) => {
     try {
       const column = await axios.delete('/api/column/delete', { data: { columnId: id } }) as ColumnType;
-      dispatch({ type: 'delete', column });
+      updateColumn(column);
     } catch (error) {
       console.error('Failed to delete column:', error);
     }}
