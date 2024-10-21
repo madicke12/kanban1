651ee18e6bb9052ab9a7be302e50e9083081b84f
madicke12 <cissemadicke8@gmail.com>
2024-10-21 20:11:43 +0000

    Refactor column create route to use Prisma for database operations

--- src/app/api/column/create/route.ts
+++ src/app/api/column/create/route.ts
@@ -1,6 +1,22 @@
-import { NextResponse } from "next/server"
+import prisma from '@/app/lib/prismaSingleton';
+import { NextResponse } from "next/server";
 
-export async function POST(){
-    const mes ='asas'
-    return new   NextResponse(mes , { status: 200 })
+export async function POST(req: Request) {
+    const body = await req.json()
+    const {name , boardId} = body
+    console.log('name', name)
+    console.log('boardId', boardId)
+
+    try {
+        const column  = await prisma.column.create({
+            data:{
+                name,
+                boardId
+            }
+        })
+        return NextResponse.json(column, { status: 200 })
+    } catch (error) {
+        console.error('Failed to create column:', error);
+        return NextResponse.json({error}, { status: 500 });
+    }
 }
