15d008632adfdf6f687766123fe1c12051a1abf6
madicke12 <cissemadicke8@gmail.com>
2024-10-24 15:53:12 +0000

    Refactor addBoardDialog component to use SidebarGroupAction for adding a project

--- src/components/addBoardDialog.tsx
+++ src/components/addBoardDialog.tsx
@@ -2,13 +2,16 @@ import { Button } from "@/components/ui/button";
 import { Dialog, DialogContent, DialogTrigger } from "@/components/ui/dialog";
 import { PlusIcon } from "@heroicons/react/24/solid";
 import { AddBoardForm } from "./addBoardForm";
+import { Plus } from "lucide-react";
+import { SidebarGroupAction } from "./ui/sidebar";
 
 export function AddBoardDialog() {
   return (
     <Dialog>
       <DialogTrigger asChild>
-      <Button className=' bg-primary text-white hover:text-white hover:bg-primary-500 font-bold border-none ' variant={'outline'}><PlusIcon width={20} height={20}/><span>Add Board</span></Button>
-
+      <SidebarGroupAction title="Add Project">
+        <Plus /> <span className="sr-only">Add Board</span>
+      </SidebarGroupAction>
       </DialogTrigger>
       <DialogContent className="sm:max-w-[425px]">
         <AddBoardForm/>
