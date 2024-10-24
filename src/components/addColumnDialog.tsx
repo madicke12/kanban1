4a01c7fa2b5f76a264745ea76e15499b7c835151
madicke12 <cissemadicke8@gmail.com>
2024-10-24 15:53:17 +0000

    Refactor AddColumnDialog component to use a button with fixed width and height

--- src/components/addColumnDialog.tsx
+++ src/components/addColumnDialog.tsx
@@ -3,18 +3,15 @@ import { Dialog, DialogContent, DialogTrigger } from "@/components/ui/dialog";
 import { PlusIcon } from "@heroicons/react/24/solid";
 import AddColumnForm from "./addColumnForm";
 
-
-export function AddColumnDialog({id}:{id:string}) {
+export const AddColumnDialog: React.FC<{ id: string }> = ({ id }) => {
   return (
     <Dialog>
       <DialogTrigger asChild>
-      <Button className='w-full max-w-[220px] bg-primary flex items-center justify-center h-[100vh] font-bold'>Add Column</Button>
-
-
+        <Button className="w-64 h-12">Add Column</Button>
       </DialogTrigger>
       <DialogContent className="sm:max-w-[425px]">
         <AddColumnForm id={id}/>
       </DialogContent>
     </Dialog>
   );
-}
+};
