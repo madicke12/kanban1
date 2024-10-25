257b9c8d43d3e2c845a616f93cb8b5da8e6a7645
madicke12 <cissemadicke8@gmail.com>
2024-10-25 22:26:29 +0000

    Refactor NextAuth configuration to use separate authConfig object

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -4,23 +4,30 @@ import NextAuth, { Session } from "next-auth"
 import GitHub from "next-auth/providers/github"
 import GoogleProvider from "next-auth/providers/google"
 
-const handler = NextAuth({
-    adapter: PrismaAdapter(prisma),
-    providers: [
-        GitHub({
-            clientId: process.env.GITHUB_ID as string,
-            clientSecret: process.env.GITHUB_SECRET as string,
-        }),
-        GoogleProvider({
-            clientId: process.env.GOOGLE_CLIENT_ID as string,
-            clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
-          })
-    ], callbacks: {
-        session: ({ session, user }: { session: Session, user :any}) => ({
-          ...session,
-          user,
-        }),
-      },
-    });
+export const authConfig ={
+  adapter: PrismaAdapter(prisma),
+  secret: process.env.SECRET,
+  providers: [
+      GitHub({
+          clientId: process.env.GITHUB_ID as string,
+          clientSecret: process.env.GITHUB_SECRET as string,
+      }),
+      GoogleProvider({
+          clientId: process.env.GOOGLE_CLIENT_ID as string,
+          clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
+        })
+  ], callbacks: {
+      session: ({ session, user }: { session: Session, user :any}) => ({
+        ...session,
+        user,
+      }),
+    },
+    jwt: {
+      secret: process.env.SECRET,
+    },
+  
+  }
+
+const handler = NextAuth(authConfig);
 
 export { handler as GET, handler as POST }
