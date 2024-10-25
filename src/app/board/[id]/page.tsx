aeb633863ee84753729ad1236180b7017a5a42d5
madicke12 <cissemadicke8@gmail.com>
2024-10-25 22:31:39 +0000

    Refactor Board page component to use Suspense for lazy loading of Board component

--- src/app/board/[id]/page.tsx
+++ src/app/board/[id]/page.tsx
@@ -1,12 +1,15 @@
 
 import Board from '@/components/board';
 import { ParamsType } from '../../lib/types/itemTypes';
+import { Suspense } from 'react';
+import ColumnSkeleton from '@/components/columnSkeleton';
 
-export default function page({ params }: { params: ParamsType }) {
+export default async function page({ params }: { params: ParamsType }) {
   return (
     <div className=''>
-
+        <Suspense fallback={<ColumnSkeleton/>} >
         <Board id={params.id} />
+        </Suspense>
     </div>
   );
 }
