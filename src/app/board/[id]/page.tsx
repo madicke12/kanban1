4e9b9a710f358d511deee7ab97ba1b87ebe1ec11
madicke12 <cissemadicke8@gmail.com>
2024-10-28 21:01:05 +0000

    Refactor page component to import Suspense from react and ParamsType from itemTypes

--- src/app/board/[id]/page.tsx
+++ src/app/board/[id]/page.tsx
@@ -1,14 +1,15 @@
 
 import Board from '@/components/board';
-import { ParamsType } from '../../lib/types/itemTypes';
-import { Suspense } from 'react';
 import ColumnSkeleton from '@/components/columnSkeleton';
+import { Suspense } from 'react';
+import { ParamsType } from '../../lib/types/itemTypes';
 
 export default async function page({ params }: { params: ParamsType }) {
   return (
     <div className=''>
         <Suspense fallback={<ColumnSkeleton/>} >
         <Board id={params.id} />
+
         </Suspense>
     </div>
   );
