67dba65fa15c94dd7695eec53abfc2674394904b
madicke12 <cissemadicke8@gmail.com>
2024-10-28 21:00:46 +0000

    Add Toaster component to display toast notifications

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import localFont from "next/font/local";
 import SessionProvider from './sessionprovider';
 import { Analytics } from "@vercel/analytics/react"
 import "./globals.css";
+import { Toaster } from "@/components/ui/toaster";
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
@@ -31,6 +32,7 @@ export default function RootLayout({
         >
 
           {children}
+          <Toaster />
         </body>
       </SessionProvider>
     </html>
