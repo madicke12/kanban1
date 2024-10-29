1911533d49a966a3a6eeed9cf1d4912040529075
madicke12 <cissemadicke8@gmail.com>
2024-10-29 23:35:53 +0000

    Refactor TaskCard component to include TaskCollapsible

--- src/components/taskCard.tsx
+++ src/components/taskCard.tsx
@@ -1,6 +1,7 @@
 import { TaskType } from '@/app/lib/types/itemTypes'
 import Image from 'next/image'
 import { useDrag } from 'react-dnd'
+import { TaskCollapsible } from './tasksCollapsible'
 
 export default function TaskCard({ task }: { task: TaskType }) {
 const [{isDragging}, drag] = useDrag(() => ({
@@ -12,6 +13,7 @@ const [{isDragging}, drag] = useDrag(() => ({
     task.description= 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla nec purus feugiat, molestie ipsum et, ultricies nunc. Nullam nec purus nec purus feugiat, molestie ipsum et, ultricies nunc. Nullam nec purus'
   return (
     <div className={` bg-white w-[268px] p-[10px] flex flex-col gap-3 items-start rounded-[4px] shadow-custom relative ${isDragging ? 'opacity-50':''} `}  ref={drag as unknown as React.LegacyRef<HTMLDivElement> }>
+      
       {task.picture && <Image src={task.picture} loading='lazy' width={246} height={246} className='rounded-sm w-[246px] h-[230px] ' alt='task banner' />}
       <div className='mt-4'>
         <span className='font-bold'>{task.titre}</span>
