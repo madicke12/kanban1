89a6cd0ee650c118a11505c702b8b03e7547302a
madicke12 <cissemadicke8@gmail.com>
2024-10-29 23:35:58 +0000

    Refactor TaskCollapsible component to include collapsible functionality and UI elements

---
+++ src/components/tasksCollapsible.tsx
@@ -0,0 +1,35 @@
+"use client"
+
+import { Button } from "@/components/ui/button";
+import {
+    Collapsible,
+    CollapsibleContent,
+    CollapsibleTrigger,
+} from "@/components/ui/collapsible";
+import { ChevronsUpDown } from "lucide-react";
+import * as React from "react";
+
+export function TaskCollapsible() {
+  const [isOpen, setIsOpen] = React.useState(false)
+
+  return (
+    <Collapsible
+      open={isOpen}
+      onOpenChange={setIsOpen}
+      className="w-[350px] space-y-2"
+    >
+      <div className="flex items-center justify-between space-x-4 px-4">
+        <CollapsibleTrigger asChild>
+            <ChevronsUpDown className="ml-auto size-4" />
+
+        </CollapsibleTrigger>
+      </div>
+      <div className="rounded-md border px-4 py-2 font-mono text-sm shadow-sm">
+        @radix-ui/primitives
+      </div>
+      <CollapsibleContent className="space-y-2">
+       
+      </CollapsibleContent>
+    </Collapsible>
+  )
+}
