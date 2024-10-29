1f8301c286f0deecec5b10a16528ecab6842d4a9
madicke12 <cissemadicke8@gmail.com>
2024-10-29 23:35:44 +0000

    Refactor NavUser component to include sign out functionality

--- src/components/nav-user.tsx
+++ src/components/nav-user.tsx
@@ -24,6 +24,7 @@ import {
   SidebarMenuItem,
   useSidebar,
 } from "@/components/ui/sidebar"
+import { signOut } from "next-auth/react"
 
 export function NavUser({
   session,
@@ -91,7 +92,7 @@ export function NavUser({
               </DropdownMenuItem>
             </DropdownMenuGroup>
             <DropdownMenuSeparator /> */}
-            <DropdownMenuItem>
+            <DropdownMenuItem onClick={()=>signOut()}>
               <LogOut />
               Log out
             </DropdownMenuItem>
