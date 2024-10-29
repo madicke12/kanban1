851efe393ec1dee2297f56d807e4a34946b7f24d
madicke12 <cissemadicke8@gmail.com>
2024-10-29 23:34:47 +0000

    Refactor TaskContextMenu component to include delete functionality

---
+++ src/components/contexmenu.tsx
@@ -0,0 +1,33 @@
+import {
+    ContextMenu,
+    ContextMenuContent,
+    ContextMenuItem,
+    ContextMenuTrigger,
+  } from "@/components/ui/context-menu"
+import TaskModal from "./taskDialog"
+import { TaskType } from "@/app/lib/types/itemTypes"
+import { deleteItem } from "@/lib/utils"
+import { useTaskStore } from "@/app/boardContext"
+import axios from "axios"
+import { revalidatePath } from "next/cache"
+
+
+const TaskContextMenu = ({task}:{task:TaskType}) => {
+    const del = useTaskStore((state) => state.deleteTask)
+    const handleDelete = async (id: string) => {
+        del(id)
+        await axios.delete(`/api/task/delete/${id}`)
+      }
+
+    return(
+        <ContextMenu>
+        <ContextMenuTrigger><TaskModal task={task}/></ContextMenuTrigger>
+        <ContextMenuContent>
+          <ContextMenuItem>Modifier</ContextMenuItem>
+          <ContextMenuItem onClick={()=>handleDelete(task.id)}>Supprimer</ContextMenuItem>
+        </ContextMenuContent>
+      </ContextMenu>
+    )
+}
+
+export default TaskContextMenu
