2f97368577a948a67ed45ad2e6dc71274692d561
madicke12 <cissemadicke8@gmail.com>
2024-10-29 23:35:33 +0000

    Refactor NavMain component to include board list and dropdown menu

--- src/components/nav-main.tsx
+++ src/components/nav-main.tsx
@@ -13,62 +13,74 @@ import {
   SidebarGroup,
   SidebarGroupLabel,
   SidebarMenu,
+  SidebarMenuAction,
   SidebarMenuButton,
   SidebarMenuItem,
   SidebarMenuSub,
   SidebarMenuSubButton,
-  SidebarMenuSubItem,
+  SidebarMenuSubItem
 } from "@/components/ui/sidebar"
 import Link from "next/link"
 import { useEffect } from "react"
 import { AddBoardDialog } from "./addBoardDialog"
-import { Skeleton } from "./ui/skeleton"
+import { DropdownMenu, DropdownMenuContent, DropdownMenuItem, DropdownMenuTrigger } from "./ui/dropdown-menu"
 
-export function NavMain({session}: {session: {user?: any , expires:string} }) {
-  const fetchboards = useBoardStore((state) => state.fetchBoards) 
-  const user = session.user
+export function NavMain({ board }: { board: BoardType[] }) {
+  const setBoard = useBoardStore((state) => state.init)
   useEffect(() => {
-    fetchboards()
-  }, [fetchboards])
-
-  const board = useBoardStore((state) => state.boards).filter((b: BoardType) => b.userId === user.id)
+    setBoard(board)
+  }, [board])
+  const boardList = useBoardStore((state) => state.boards)
   return (
     <SidebarGroup>
       <SidebarGroupLabel>Board Liste</SidebarGroupLabel>
       <AddBoardDialog />
       <SidebarMenu>
-          <Collapsible
-            key={"ajksdvnaov"}
-            asChild
-            defaultOpen={true}
-            className="group/collapsible"
-          >
-            <SidebarMenuItem>
-              <CollapsibleTrigger asChild>
-                <SidebarMenuButton tooltip="View Boards Liste">
-                  <BookOpen/>
-                  <span>View Board Liste</span>
-                  <ChevronRight className="ml-auto transition-transform duration-200 group-data-[state=open]/collapsible:rotate-90" />
-                </SidebarMenuButton>
-              </CollapsibleTrigger>
-              <CollapsibleContent>
-              { board ? (
-                <SidebarMenuSub>
-                  {board.map((subItem) => (
-                    <SidebarMenuSubItem key={subItem.id}>
-                      <SidebarMenuSubButton asChild>
-                        <Link href={`/board/${subItem.id}`}>
-                          <span>{subItem.name}</span>
-                        </Link>
-                      </SidebarMenuSubButton>
-                    </SidebarMenuSubItem>
-                  ))}
-                </SidebarMenuSub>) :( <Skeleton />)
-}
-
-              </CollapsibleContent>
-            </SidebarMenuItem>
-          </Collapsible>
+        <Collapsible
+          key={"ajksdvnaov"}
+          asChild
+          defaultOpen={true}
+          className="group/collapsible"
+        >
+          <SidebarMenuItem>
+            <CollapsibleTrigger asChild>
+              <SidebarMenuButton tooltip="View Boards Liste">
+                <BookOpen />
+                <span>View Board Liste</span>
+                <ChevronRight className="ml-auto transition-transform duration-200 group-data-[state=open]/collapsible:rotate-90" />
+              </SidebarMenuButton>
+            </CollapsibleTrigger>
+            <CollapsibleContent>
+              <SidebarMenuSub>
+                {boardList.map((subItem) => (
+                  <SidebarMenuSubItem key={subItem.id}>
+                    <SidebarMenuSubButton asChild>
+                      <Link href={`/board/${subItem.id}`}>
+                        {/* <SidebarGroupLabel>{subItem.name}</SidebarGroupLabel> */}
+                        <span>{subItem.name}</span>
+                      </Link>
+                    </SidebarMenuSubButton>
+                    <DropdownMenu>
+                      <DropdownMenuTrigger asChild>
+                        <SidebarMenuAction>
+                          {/* <MoreHorizontal /> */} ...
+                        </SidebarMenuAction>
+                      </DropdownMenuTrigger>
+                      <DropdownMenuContent side="right" align="start">
+                        <DropdownMenuItem>
+                          <span>Edit Project</span>
+                        </DropdownMenuItem>
+                        <DropdownMenuItem>
+                          <span>Delete Project</span>
+                        </DropdownMenuItem>
+                      </DropdownMenuContent>
+                    </DropdownMenu>
+                  </SidebarMenuSubItem>
+                ))}
+              </SidebarMenuSub>
+            </CollapsibleContent>
+          </SidebarMenuItem>
+        </Collapsible>
       </SidebarMenu>
     </SidebarGroup>
   )
