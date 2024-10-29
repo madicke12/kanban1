07bd0eef607585377a93f6473218dd4371fc53c0
madicke12 <cissemadicke8@gmail.com>
2024-10-29 23:36:36 +0000

    Refactor utils.ts to include a deleteItem function for deleting a task

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,6 +1,16 @@
+import prisma from "@/app/lib/prismaSingleton"
 import { clsx, type ClassValue } from "clsx"
 import { twMerge } from "tailwind-merge"
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
+}
+
+
+export const deleteItem = async (id: string): Promise<void> => {
+  await prisma.task.delete({
+    where: {
+      id: id,
+    },
+  }) 
 }
