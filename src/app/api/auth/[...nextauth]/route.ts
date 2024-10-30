df34ff35a0925d7dc2c9d131122dac20b59d35f9
madicke12 <cissemadicke8@gmail.com>
2024-10-30 18:50:10 +0000

    Refactor auth route.ts to use authOption from utils

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,33 +1,8 @@
-import prisma from '@/app/lib/prismaSingleton';
-import { PrismaAdapter } from '@next-auth/prisma-adapter';
-import NextAuth, { Session } from "next-auth"
-import GitHub from "next-auth/providers/github"
-import GoogleProvider from "next-auth/providers/google"
+import { authOption } from '@/lib/utils';
+import NextAuth from "next-auth";
 
-export const authOption ={
-  adapter: PrismaAdapter(prisma),
-  secret: process.env.SECRET,
-  providers: [
-      GitHub({
-          clientId: process.env.GITHUB_ID as string,
-          clientSecret: process.env.GITHUB_SECRET as string,
-      }),
-      GoogleProvider({
-          clientId: process.env.GOOGLE_CLIENT_ID as string,
-          clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
-        })
-  ], callbacks: {
-      session: ({ session, user }: { session: Session, user :any}) => ({
-        ...session,
-        user,
-      }),
-    },
-    jwt: {
-      secret: process.env.SECRET,
-    },
-  
-  }
+
 
 const handler = NextAuth(authOption);
 
-export { handler as GET, handler as POST }
+export { handler as GET, handler as POST };
