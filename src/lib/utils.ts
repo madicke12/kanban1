135496740a41b829172326cbdd1b20e16b29725d
madicke12 <cissemadicke8@gmail.com>
2024-10-30 18:50:37 +0000

    Refactor utils.ts to include authentication options for GitHub and Google providers

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,6 +1,10 @@
 import prisma from "@/app/lib/prismaSingleton"
+import { PrismaAdapter } from "@next-auth/prisma-adapter"
 import { clsx, type ClassValue } from "clsx"
+import { NextAuthOptions, Session } from "next-auth"
 import { twMerge } from "tailwind-merge"
+import  GoogleProvider  from "next-auth/providers/google"
+import GitHub from "next-auth/providers/github"
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
@@ -14,3 +18,31 @@ export const deleteItem = async (id: string): Promise<void> => {
     },
   }) 
 }
+
+
+
+export const authOption: NextAuthOptions ={
+  adapter: PrismaAdapter(prisma),
+  secret: process.env.SECRET,
+  providers: [
+      GitHub({
+          clientId: process.env.GITHUB_ID as string,
+          clientSecret: process.env.GITHUB_SECRET as string,
+      }),
+      GoogleProvider({
+          clientId: process.env.GOOGLE_CLIENT_ID as string,
+          clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
+        })
+  ], callbacks: {
+      session: ({ session, user }: { session: Session, user :any}) => ({
+        ...session,
+        user,
+      }),
+    },
+    jwt: {
+      secret: process.env.SECRET,
+    },
+  
+  }
+
+
